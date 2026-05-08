98fe1806bfa27b7777fa5d9c9a76059fe95f8320
imbomsod <100419475+imbomsod@users.noreply.github.com>
2026-05-08 18:33:40 +0800

    feat:add GameControl

--- chapter02/part02/src/index.ts
+++ chapter02/part02/src/index.ts
@@ -1,20 +1,19 @@
 // 引入样式
 import './style/index.less'
-import Food from './modules/Food'
-import ScorePanel from './modules/ScorePanel'
-
-
+import GameControl from './modules/GameControl'
 
 // 测试代码
-const food = new Food()
-console.log(food.X, food.Y)
-food.change()
-console.log(food.X, food.Y)
+// const food = new Food()
+// console.log(food.X, food.Y)
+// food.change()
+// console.log(food.X, food.Y)
+// const scorePanel = new ScorePanel(100, 2)
+// for (let i = 0; i < 100; i++) {
+//   scorePanel.addScore()
+// }
 
+const gameControl = new GameControl()
 
-
-
-const scorePanel = new ScorePanel(100, 2)
-for (let i = 0; i < 100; i++) {
-  scorePanel.addScore()
-}
+// setInterval(() => {
+//   console.log(gameControl.direction)
+// }, 1000)
